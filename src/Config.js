e739aba67f8af41af68ebc09747c3f71a5702a7f
Sourabh Verma <vermaanurag5542@gmail.com>
2023-08-12 13:18:35 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,5 @@
 // const baseurl = "http://localhost:8000";
-const baseurl = "https://smseduca.onrender.com";
+// const baseurl = "https://smseduca.onrender.com";
+const baseurl = "http://ec2-35-85-13-5.us-west-2.compute.amazonaws.com";
 
 module.exports = baseurl;
